f24c6eab01d747e02871e55f97206ced70981c0c
16yuki0702 <hs19870702@gmail.com>
2020-05-06 16:37:34 +0000

    Fix indent

--- src/main/java/com/sample/App.java
+++ src/main/java/com/sample/App.java
@@ -7,14 +7,13 @@ import org.kie.api.runtime.KieSession;
 import com.sample.model.Sample;
 
 public class App {
-
     public static final void main(String[] args) {
         try {
-	        KieServices ks = KieServices.Factory.get();
-    	    KieContainer kContainer = ks.getKieClasspathContainer();
-        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+            KieServices ks = KieServices.Factory.get();
+            KieContainer kContainer = ks.getKieClasspathContainer();
+            KieSession kSession = kContainer.newKieSession("ksession-rules");
 
-        	Sample sample = new Sample(100);
+            Sample sample = new Sample(100);
             kSession.insert(sample);
             kSession.fireAllRules();
         } catch (Throwable t) {
